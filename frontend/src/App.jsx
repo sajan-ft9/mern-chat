f4313477bad4718c55e7b480cd85389570fb157b
Sajan Khadka <sajankhad2@gmail.com>
2023-09-10 09:05:46 +0545

    first commitreact started

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from '@chakra-ui/react'
+import './App.css'
+import { Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <>
+      <Route path='/' component="" />
+      <Route path='/chats' />
+    </>
+  )
+}
+
+export default App
